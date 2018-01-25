3bf0148f4dbd360f9e02222eb35f1f085edd9d97
Lucas Loreggia <lloreggia@gmail.com>
2018-01-26 00:00:44 +0100

    close to task bar

--- Micser.Core/MainShell.xaml.cs
+++ Micser.Core/MainShell.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Input;
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Micser.Core
 {
@@ -7,14 +9,29 @@ namespace Micser.Core
     /// </summary>
     public partial class MainShell
     {
+        private bool _isExiting;
+
         public MainShell()
         {
             InitializeComponent();
         }
 
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            e.Cancel = !_isExiting;
+            base.OnClosing(e);
+            Hide();
+        }
+
         private void CloseCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
+            _isExiting = true;
             Close();
+        }
+
+        private void OnRestoreWindow(object sender, RoutedEventArgs e)
+        {
+            Show();
         }
     }
 }
