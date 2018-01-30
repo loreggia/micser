2b6d34f0881b3cbd4abf69e8f1765838fe0ef967
Lucas Loreggia <lloreggia@gmail.com>
2018-01-30 23:33:37 +0100

    widget panel

---
+++ Micser.Main/Controls/WidgetPanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Micser.Main.Controls
+{
+    public class WidgetPanel : Control
+    {
+        static WidgetPanel()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WidgetPanel), new FrameworkPropertyMetadata(typeof(WidgetPanel)));
+        }
+    }
+}
--- Micser.Main/MainModule.cs
+++ Micser.Main/MainModule.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Micser.Core.Views;
 using Micser.Infrastructure.Extensions;
+using Micser.Infrastructure.Themes;
 using Micser.Main.ViewModels;
 using Prism.Modularity;
 using Prism.Regions;
@@ -12,15 +13,20 @@ namespace Micser.Main
     {
         private readonly IUnityContainer _container;
         private readonly IRegionManager _regionManager;
+        private readonly IResourceRegistry _resourceRegistry;
 
-        public MainModule(IUnityContainer container, IRegionManager regionManager)
+        public MainModule(IUnityContainer container, IRegionManager regionManager, IResourceRegistry resourceRegistry)
         {
             _container = container;
             _regionManager = regionManager;
+            _resourceRegistry = resourceRegistry;
         }
 
         public void Initialize()
         {
+            _resourceRegistry.Add(new Uri("Micser.Main;component/Themes/Generic.xaml", UriKind.Relative));
+            _resourceRegistry.Add(new Uri("Micser.Main;component/Themes/WidgetPanel.xaml", UriKind.Relative));
+
             _container.RegisterView<MainView, MainViewModel>("MainRegion");
 
             _regionManager.RequestNavigate("MainRegion", new Uri(nameof(MainView), UriKind.Relative));
