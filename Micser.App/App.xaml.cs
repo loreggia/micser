1303272a103ff4b429a458767d37c9f3753b2f72
Lucas Loreggia <lloreggia@gmail.com>
2018-01-30 22:57:09 +0100

    uwp project

---
+++ Micser.App/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+
+namespace Micser.App
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate("MainShell", null);
+
+            Window.Current.Activate();
+            return Task.FromResult<object>(null);
+        }
+    }
+}
