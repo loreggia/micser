54ce115317433bdbb5d7ac71f075488c143d81e2
Lucas Loreggia <lloreggia@gmail.com>
2018-02-09 10:10:49 +0100

    fixed toolbox

--- Micser.Main/Themes/ResourceManager.cs
+++ Micser.Main/Themes/ResourceManager.cs
@@ -15,9 +15,15 @@ namespace Micser.Main.Themes
                 {
                     _sharedDictionary = new ResourceDictionary();
                     var generic = (ResourceDictionary)Application.LoadComponent(new Uri("/Micser.Main;component/Themes/Generic.xaml", UriKind.Relative));
+                    var connection = (ResourceDictionary)Application.LoadComponent(new Uri("/Micser.Main;component/Themes/Connection.xaml", UriKind.Relative));
+                    var connector = (ResourceDictionary)Application.LoadComponent(new Uri("/Micser.Main;component/Themes/Connector.xaml", UriKind.Relative));
+                    var thumbs = (ResourceDictionary)Application.LoadComponent(new Uri("/Micser.Main;component/Themes/Thumbs.xaml", UriKind.Relative));
                     var widget = (ResourceDictionary)Application.LoadComponent(new Uri("/Micser.Main;component/Themes/Widget.xaml", UriKind.Relative));
                     var widgetPanel = (ResourceDictionary)Application.LoadComponent(new Uri("/Micser.Main;component/Themes/WidgetPanel.xaml", UriKind.Relative));
                     _sharedDictionary.MergedDictionaries.Add(generic);
+                    _sharedDictionary.MergedDictionaries.Add(connection);
+                    _sharedDictionary.MergedDictionaries.Add(connector);
+                    _sharedDictionary.MergedDictionaries.Add(thumbs);
                     _sharedDictionary.MergedDictionaries.Add(widget);
                     _sharedDictionary.MergedDictionaries.Add(widgetPanel);
                 }
