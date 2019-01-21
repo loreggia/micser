0bad1eda73964f60c7d375e19c364bf4788f76d5
Lucas Loreggia <lloreggia@gmail.com>
2019-01-21 17:44:20 +0100

    Update modules api

--- src/Micser.Common.Test/DataAccess/DatabaseTest.cs
+++ src/Micser.Common.Test/DataAccess/DatabaseTest.cs
@@ -24,22 +24,12 @@ namespace Micser.Infrastructure.Test.DataAccess
 
         private class TestClass
         {
-            public TestClass()
-            {
-                Id = Guid.NewGuid();
-            }
-
-            public Guid Id
-            {
-                get;
-                // ReSharper disable once AutoPropertyCanBeMadeGetOnly.Local
-                // ReSharper disable once MemberCanBePrivate.Local
-                set;
-            }
+            public Guid Id{get;set;}
 
             public IDictionary<string, object> Dictionary { get; set; }
             public int Integer { get; set; }
             public string String { get; set; }
+            public DateTime DateTime { get; set; }
         }
 
         [Fact]
@@ -65,18 +55,21 @@ namespace Micser.Infrastructure.Test.DataAccess
             var fileName = _testFileManager.GetFileName();
             var db = new Database(fileName, LogManager.CreateNullLogger());
 
-            Guid id;
+            var id = Guid.NewGuid();
+            var now = DateTime.Now;
 
             using (var saveContext = db.GetContext())
             {
                 var testObject = new TestClass
                 {
+                    Id = id,
                     Dictionary = new Dictionary<string, object>
                     {
                         {"key", "value"}
                     },
                     Integer = 42,
-                    String = "Test"
+                    String = "Test",
+                    DateTime = now
                 };
                 id = testObject.Id;
 
@@ -95,6 +88,7 @@ namespace Micser.Infrastructure.Test.DataAccess
                 Assert.Equal(1, result.Dictionary.Count);
                 Assert.True(result.Dictionary.ContainsKey("key"));
                 Assert.Equal("value", result.Dictionary["key"]);
+                Assert.Equal(now, result.DateTime);
             }
         }
     }
