ddce1798fb6f15e1485467fb9c3247f2f1b6a0b1
Lucas Loreggia <lloreggia@gmail.com>
2019-01-24 22:22:48 +0100

    Project/Build improvements

--- src/Micser.Setup/Program.cs
+++ src/Micser.Setup/Program.cs
@@ -8,35 +8,37 @@ namespace Micser.Setup
     {
         private static void Main()
         {
+            bool FileFilter(string file)
+            {
+                return !file.EndsWith(".test.dll", StringComparison.InvariantCultureIgnoreCase) &&
+                       !file.EndsWith(".xml", StringComparison.InvariantCultureIgnoreCase) &&
+                       !file.EndsWith(".pdb", StringComparison.InvariantCultureIgnoreCase);
+            }
+
             var project =
                 new ManagedProject("Micser",
                                    new Dir(@"%ProgramFiles%\Micser",
-                                           new Dir(@"App",
-                                                   new Files(@"App\*.*",
-                                                             file => !file.EndsWith(".test.dll", StringComparison.InvariantCultureIgnoreCase) &&
-                                                                     !file.EndsWith(".xml", StringComparison.InvariantCultureIgnoreCase) &&
-                                                                     !file.EndsWith(".pdb", StringComparison.InvariantCultureIgnoreCase))),
-                                           new Dir(@"Driver",
-                                                   new Files(@"Driver\Micser.Vac.Package\*.*"))),
+                                           new Dir(@"App", new Files(@"App\*.*", FileFilter)),
+                                           //new Dir(@"Engine", new Files(@"Engine\*.*", FileFilter)),
+                                           new Dir(@"Driver", new Files(@"Driver\Win32\*.*"))),
                                    new Dir(@"%ProgramMenu%\Micser",
-                                           new ExeFileShortcut("Micser",
-                                                               @"[INSTALLDIR]\App\Micser.Core.exe", ""),
-                                           new ExeFileShortcut("Uninstall Micser", "[System64Folder]msiexec.exe", "/x [ProductCode]")))
+                                       new ExeFileShortcut("Micser", @"[INSTALLDIR]\App\Micser.App.exe", ""),
+                                       new ExeFileShortcut("Uninstall Micser", "[SystemFolder]msiexec.exe", "/x [ProductCode]")))
                 {
                     GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b"),
                     ManagedUI = new ManagedUI()
                 };
 
             project.ManagedUI.InstallDialogs.Add(Dialogs.Welcome)
-                   .Add(Dialogs.Licence)
-                    //.Add(Dialogs.SetupType)
-                    //.Add(Dialogs.Features)
+                   //.Add(Dialogs.Licence)
+                   //.Add(Dialogs.SetupType)
+                   //.Add(Dialogs.Features)
                    .Add(Dialogs.InstallDir)
                    .Add(Dialogs.Progress)
                    .Add(Dialogs.Exit);
 
             project.ManagedUI.ModifyDialogs.Add(Dialogs.MaintenanceType)
-                    //.Add(Dialogs.Features)
+                   //.Add(Dialogs.Features)
                    .Add(Dialogs.Progress)
                    .Add(Dialogs.Exit);
 
