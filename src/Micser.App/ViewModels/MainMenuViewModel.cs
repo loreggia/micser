e0159aa08ff1462707a719dc80ee51b75976995e
Lucas Loreggia <lucas.loreggia@netiva.ch>
2019-02-13 15:03:42 +0100

    removed debug code

--- src/Micser.App/ViewModels/MainMenuViewModel.cs
+++ src/Micser.App/ViewModels/MainMenuViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Micser.App.Infrastructure;
+﻿using Micser.App.Infrastructure;
 using Micser.App.Infrastructure.Menu;
 using System.Collections.Generic;
 
@@ -9,48 +8,6 @@ namespace Micser.App.ViewModels
     {
         private readonly IMenuItemRegistry _menuItemRegistry;
         private IEnumerable<TreeNode<MenuItemDescription>> _menuItems;
-
-        public MainMenuViewModel()
-        {
-            var menuItems = new List<MenuItemDescription>()
-            {
-                new MenuItemDescription
-                {
-                    Header = "Header",
-                    Id = "Top"
-                },
-                new MenuItemDescription
-                {
-                    Header = "Sub 1",
-                    Id = "Sub1",
-                    ParentId = "Top",
-                    Order = 1
-                },
-                new MenuItemDescription
-                {
-                    Header = "Separator",
-                    IsSeparator = true,
-                    ParentId = "Top",
-                    Order = 2
-                },
-                new MenuItemDescription
-                {
-                    Header = "Sub 2",
-                    Id = "Sub2",
-                    ParentId = "Top",
-                    Order = 3
-                },
-                new MenuItemDescription
-                {
-                    Header = "Sub 2 Sub 1",
-                    Id = "Sub2Sub1",
-                    ParentId = "Top",
-                    Order = 1
-                }
-            };
-
-            MenuItems = CreateTree(menuItems);
-        }
 
         public MainMenuViewModel(IMenuItemRegistry menuItemRegistry)
         {
@@ -63,17 +20,17 @@ namespace Micser.App.ViewModels
             set => SetProperty(ref _menuItems, value);
         }
 
-        private static IEnumerable<TreeNode<MenuItemDescription>> CreateTree(IEnumerable<MenuItemDescription> menuItems)
-        {
-            return TreeNode<MenuItemDescription>.CreateTree(menuItems, m => m.Id, m => m.ParentId, m => m.Order);
-        }
-
         protected override void OnNavigatedTo(object parameter)
         {
             base.OnNavigatedTo(parameter);
 
             var menuItems = _menuItemRegistry.Items;
             MenuItems = CreateTree(menuItems);
+        }
+
+        private static IEnumerable<TreeNode<MenuItemDescription>> CreateTree(IEnumerable<MenuItemDescription> menuItems)
+        {
+            return TreeNode<MenuItemDescription>.CreateTree(menuItems, m => m.Id, m => m.ParentId, m => m.Order);
         }
     }
 }
