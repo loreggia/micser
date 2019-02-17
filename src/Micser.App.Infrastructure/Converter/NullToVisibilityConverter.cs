dfa824b20110fa1f4d72e0c13f79235b9ae187ec
Lucas Loreggia <lloreggia@gmail.com>
2019-02-17 13:32:48 +0100

    Fixed tool bar icons

---
+++ src/Micser.App.Infrastructure/Converter/NullToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace Micser.App.Infrastructure.Converter
+{
+    public class NullToVisibilityConverter : ConverterExtension
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
