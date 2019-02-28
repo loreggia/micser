ef3d7353c99882a2a5398da9848070bf97175b19
Lucas Loreggia <lucas.loreggia@netiva.ch>
2019-02-28 17:09:57 +0100

    Settings description button

---
+++ src/Micser.App.Infrastructure/Converter/RelativeCenterPlacementConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Micser.App.Infrastructure.Converter
+{
+    public class RelativeCenterPlacementConverter : ConverterExtension
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double d)
+            {
+                return -(d / 2d);
+            }
+
+            return value;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
