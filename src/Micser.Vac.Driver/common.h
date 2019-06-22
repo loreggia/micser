037dad8458c68b934a7976fa3a0454e2382f4cc1
Lucas Loreggia <lloreggia@gmail.com>
2019-06-23 00:30:44 +0200

    Formatting

--- src/Micser.Vac.Driver/common.h
+++ src/Micser.Vac.Driver/common.h
@@ -24,26 +24,26 @@ DEFINE_GUID(IID_IAdapterCommon, 0x7eda2950, 0xbf9f, 0x11d0, 0x87, 0x1f, 0x0, 0xa
 //
 DECLARE_INTERFACE_(IAdapterCommon, IUnknown)
 {
-    STDMETHOD_(NTSTATUS, Init) (THIS_ IN PDEVICE_OBJECT DeviceObject) PURE;
-    STDMETHOD_(PDEVICE_OBJECT, GetDeviceObject) (THIS) PURE;
-    STDMETHOD_(VOID, SetWaveServiceGroup) (THIS_ IN PSERVICEGROUP ServiceGroup) PURE;
-    STDMETHOD_(NTSTATUS, InstantiateDevices) (THIS) PURE;
-    STDMETHOD_(NTSTATUS, InstantiateDevice) (THIS_ IN UINT Index) PURE;
-    STDMETHOD_(NTSTATUS, UninstantiateDevices) (THIS) PURE;
-    STDMETHOD_(BOOL, bDevSpecificRead) (THIS_) PURE;
-    STDMETHOD_(VOID, bDevSpecificWrite) (THIS_ IN BOOL bDevSpecific);
-    STDMETHOD_(INT, iDevSpecificRead) (THIS_) PURE;
-    STDMETHOD_(VOID, iDevSpecificWrite) (THIS_ IN INT iDevSpecific);
-    STDMETHOD_(UINT, uiDevSpecificRead) (THIS_) PURE;
-    STDMETHOD_(VOID, uiDevSpecificWrite) (THIS_ IN UINT uiDevSpecific);
-    STDMETHOD_(BOOL, MixerMuteRead) (THIS_ IN ULONG Index) PURE;
-    STDMETHOD_(VOID, MixerMuteWrite) (THIS_ IN ULONG Index, IN BOOL Value);
-    STDMETHOD_(ULONG, MixerMuxRead) (THIS);
-    STDMETHOD_(VOID, MixerMuxWrite) (THIS_ IN ULONG Index);
-    STDMETHOD_(LONG, MixerVolumeRead) (THIS_ IN ULONG Index, IN LONG Channel) PURE;
-    STDMETHOD_(VOID, MixerVolumeWrite) (THIS_ IN ULONG Index, IN LONG Channel, IN LONG Value) PURE;
-    STDMETHOD_(VOID, MixerReset) (THIS) PURE;
-    STDMETHOD_(BOOL, IsInstantiated) (THIS) PURE;
+	STDMETHOD_(NTSTATUS, Init) (THIS_ IN PDEVICE_OBJECT DeviceObject) PURE;
+	STDMETHOD_(PDEVICE_OBJECT, GetDeviceObject) (THIS) PURE;
+	STDMETHOD_(VOID, SetWaveServiceGroup) (THIS_ IN PSERVICEGROUP ServiceGroup) PURE;
+	STDMETHOD_(NTSTATUS, InstantiateDevices) (THIS) PURE;
+	STDMETHOD_(NTSTATUS, InstantiateDevice) (THIS_ IN UINT Index) PURE;
+	STDMETHOD_(NTSTATUS, UninstantiateDevices) (THIS) PURE;
+	STDMETHOD_(BOOL, bDevSpecificRead) (THIS_) PURE;
+	STDMETHOD_(VOID, bDevSpecificWrite) (THIS_ IN BOOL bDevSpecific);
+	STDMETHOD_(INT, iDevSpecificRead) (THIS_) PURE;
+	STDMETHOD_(VOID, iDevSpecificWrite) (THIS_ IN INT iDevSpecific);
+	STDMETHOD_(UINT, uiDevSpecificRead) (THIS_) PURE;
+	STDMETHOD_(VOID, uiDevSpecificWrite) (THIS_ IN UINT uiDevSpecific);
+	STDMETHOD_(BOOL, MixerMuteRead) (THIS_ IN ULONG Index) PURE;
+	STDMETHOD_(VOID, MixerMuteWrite) (THIS_ IN ULONG Index, IN BOOL Value);
+	STDMETHOD_(ULONG, MixerMuxRead) (THIS);
+	STDMETHOD_(VOID, MixerMuxWrite) (THIS_ IN ULONG Index);
+	STDMETHOD_(LONG, MixerVolumeRead) (THIS_ IN ULONG Index, IN LONG Channel) PURE;
+	STDMETHOD_(VOID, MixerVolumeWrite) (THIS_ IN ULONG Index, IN LONG Channel, IN LONG Value) PURE;
+	STDMETHOD_(VOID, MixerReset) (THIS) PURE;
+	STDMETHOD_(BOOL, IsInstantiated) (THIS) PURE;
 };
 typedef IAdapterCommon *PADAPTERCOMMON;
 
@@ -52,13 +52,13 @@ typedef IAdapterCommon *PADAPTERCOMMON;
 //=============================================================================
 NTSTATUS NewAdapterCommon
 (
-    OUT PUNKNOWN *              Unknown,
-    IN  REFCLSID,
-    IN  PUNKNOWN                UnknownOuter OPTIONAL,
-    _When_((PoolType & NonPagedPoolMustSucceed) != 0,
-        __drv_reportError("Must succeed pool allocations are forbidden. "
-            "Allocation failures cause a system crash"))
-    IN  POOL_TYPE               PoolType
+	OUT PUNKNOWN *              Unknown,
+	IN  REFCLSID,
+	IN  PUNKNOWN                UnknownOuter OPTIONAL,
+	_When_((PoolType & NonPagedPoolMustSucceed)!=0,
+		__drv_reportError("Must succeed pool allocations are forbidden. "
+			"Allocation failures cause a system crash"))
+	IN  POOL_TYPE               PoolType
 );
 
 #endif  //_COMMON_H_
