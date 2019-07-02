d910e7344a3fe65bd98c300613f4d445e5e3dd6a
Lucas Loreggia <lloreggia@gmail.com>
2019-07-02 22:39:49 +0200

    WIP

--- src/Micser.BuildTasks/UpdateAssemblyVersion.cs
+++ src/Micser.BuildTasks/UpdateAssemblyVersion.cs
@@ -7,14 +7,16 @@ namespace Micser.BuildTasks
     public class UpdateAssemblyVersion : Task
     {
         private readonly Regex _rxVersion;
+        private readonly Regex _rxVersionFile;
 
         public UpdateAssemblyVersion()
         {
-            _rxVersion = new Regex(@"(\d+)\.(\d+)\.(\d+).(\d+)", RegexOptions.Compiled);
+            _rxVersion = new Regex(@"(\d+)\.(\d+)\.(\d+)\.(\d+)", RegexOptions.Compiled);
         }
 
         public string InputFileName { get; set; }
         public string OutputFileName { get; set; }
+        public string VersionFileName { get; set; }
 
         public override bool Execute()
         {
@@ -30,7 +32,24 @@ namespace Micser.BuildTasks
                 return false;
             }
 
-            var fileContent = File.ReadAllText(InputFileName);
+            if (!File.Exists(VersionFileName))
+            {
+                Log.LogError($"Version file '{VersionFileName}' not found.");
+                return false;
+            }
+
+            var versionContent = File.ReadAllText(VersionFileName);
+            versionContent = versionContent.Trim();
+
+            if (string.IsNullOrWhiteSpace(versionContent))
+            {
+                Log.LogError($"Invalid version file content ({VersionFileName}).");
+                return false;
+            }
+
+            var templateContent = File.ReadAllText(InputFileName);
+
+            File.WriteAllText(OutputFileName, templateContent);
         }
     }
 }
