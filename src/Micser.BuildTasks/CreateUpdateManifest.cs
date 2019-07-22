0a98e860a30533a09928d6d9bdb1ee028f0ef540
Lucas Loreggia <lloreggia@gmail.com>
2019-07-22 22:56:30 +0200

    Build task for update manifest creation WIP

---
+++ src/Micser.BuildTasks/CreateUpdateManifest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Micser.Common.Updates;
+using System;
+using System.IO;
+
+namespace Micser.BuildTasks
+{
+    public class CreateUpdateManifest : Task
+    {
+        [Required]
+        public string DescriptionFileName { get; set; }
+
+        [Required]
+        public string OutputFileName { get; set; }
+
+        public override bool Execute()
+        {
+            if (!File.Exists(DescriptionFileName))
+            {
+                Log.LogError($"The description file does not exist: {DescriptionFileName}.");
+                return false;
+            }
+
+            var description = File.ReadAllText(DescriptionFileName);
+
+            var updateManifest = new UpdateManifest
+            {
+                Date = DateTime.UtcNow,
+                Description = description,
+                FileName =
+            }
+        }
+    }
+}
