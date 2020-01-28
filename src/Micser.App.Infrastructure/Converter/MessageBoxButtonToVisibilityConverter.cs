ddb49d5d9ae34512570f91d11426011b7c0cbe76
Lucas Loreggia <lloreggia@gmail.com>
2020-01-28 20:04:57 +0100

    Fixed message box button display

---
+++ src/Micser.App.Infrastructure/Converter/MessageBoxButtonToVisibilityConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace Micser.App.Infrastructure.Converter
+{
+    public enum MessageBoxButtonType
+    {
+        Ok,
+        Yes,
+        No,
+        Cancel
+    }
+
+    public class MessageBoxButtonToVisibilityConverter : ConverterExtension
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is MessageBoxButton buttonValue &&
+                parameter is MessageBoxButtonType type)
+            {
+                var result = buttonValue switch
+                {
+                    MessageBoxButton.OK when type == MessageBoxButtonType.Ok => true,
+                    MessageBoxButton.OKCancel when type == MessageBoxButtonType.Ok || type == MessageBoxButtonType.Cancel => true,
+                    MessageBoxButton.YesNo when type == MessageBoxButtonType.Yes || type == MessageBoxButtonType.No => true,
+                    MessageBoxButton.YesNoCancel when type == MessageBoxButtonType.Yes || type == MessageBoxButtonType.No || type == MessageBoxButtonType.Cancel => true,
+                    _ => false
+                };
+
+                return result ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
